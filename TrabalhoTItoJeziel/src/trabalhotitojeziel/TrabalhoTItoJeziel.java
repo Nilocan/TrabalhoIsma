1663d0d0b91653e09d3a254822109ce3e25abe0b
Nilocan <titolianchen3341@gmail.com>
2019-06-18 07:52:50 -0300

    Add files via upload

---
+++ TrabalhoTItoJeziel/src/trabalhotitojeziel/TrabalhoTItoJeziel.java
@@ -0,0 +1,19 @@
+
+package trabalhotitojeziel;
+
+import java.util.Scanner;
+public class TrabalhoTItoJeziel {
+/*
+    JEz, o isma pediu pra nozes fazer a criptografia por rsa
+    */
+
+    public static void main(String[] args) {
+
+    String texto;
+    int c;
+    Scanner a = new Scanner(System.in);
+    texto = a.nextLine();
+    System.out.println(texto);
+    }
+    
+}
